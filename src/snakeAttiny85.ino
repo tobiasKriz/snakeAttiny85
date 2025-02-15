647cd7deed5e0546754521f22ba469b7fab567e2
tobiasKriz <tobiaskrizan@gmail.com>
2025-02-15 18:30:46 +0100

    edited rendering

--- src/snakeAttiny85.ino
+++ src/snakeAttiny85.ino
@@ -15,6 +15,16 @@ const uint8_t snakeBody[] PROGMEM = {
     0x7e,
     0x00,
 };
+const uint8_t blank[] PROGMEM = {
+    0x00,
+    0x00,
+    0x00,
+    0x00,
+    0x10,
+    0x00,
+    0x00,
+    0x00,
+};
 
 byte snakeCords[128] = {
     0b00000001, 0b00000011, 0b00000101, 0b00000111, 0b00000000, 0b00000000, 0b00000000, 0b00000000, 0b00000000, 0b00000000,
@@ -60,7 +70,7 @@ void setup()
 void loop()
 {
 
-  // oled.setCursor(40, 1);oled.print(strela);oled.setCursor(40, 1);oled.print("  ");
+  // oled.setFont(FONT8X16);oled.setCursor(40, 1);oled.print(strela);oled.setCursor(40, 1);oled.print("  ");
 
   temp = analogRead(A2);
 
@@ -70,7 +80,8 @@ void loop()
     firstPart = (snakeCords[i] >> 4) & 0x0F;
     secondPart = (snakeCords[i] >> 1) & 0x07; // Extract next 3 bits
     lastDigit = snakeCords[i] & 0x01;
-    if (lastDigit == 0 || firstPart >= 15 || secondPart >= 7)
+
+    if (firstPart >= 15 || secondPart >= 7 || lastDigit == 0)
     {
       break;
     }
@@ -114,44 +125,52 @@ void loop()
       byteWrite |= (secondPart & 0x07) << 1; // Store next 3 bits
       byteWrite |= (lastDigit & 0x01);       // Store last bit
       snakeCords[i] = byteWrite;             // Store first 4 bits
+      
     }
   }
 
-  for (int i = 0; i < 128; i++)
-  {
-    binaryInput = snakeCords[i];
+
+    binaryInput = snakeCords[0];
     firstPart = (binaryInput >> 4) & 0x0F;  // Extract first 4 bits
     secondPart = (binaryInput >> 1) & 0x07; // Extract next 3 bits
     lastDigit = binaryInput & 0x01;
-    oled.setFont(FONT6X8);
-    oled.setCursor(40, 6);
-    oled.print(secondPart);
-    if (lastDigit == 0)
-    {
-      break;
-    }
-    oled.setFont(FONT6X8);
-    oled.setCursor(10, 6);
     oled.bitmap(firstPart * 8, secondPart, 8 + firstPart * 8, 1 + secondPart, snakeBody);
-  }
+
+
+    oled.bitmap( holdingPosY * 8, holdingPosX, 8 +  holdingPosY * 8, 1 + holdingPosX, blank);
+
 
   // LEVE TLACITKO
   if (temp > 830 && temp < 845)
   {
-    snakeDirection = 0;
+    
+    if (snakeDirection == 0)
+    {
+        snakeDirection = 3;
+    }else{
+      snakeDirection--;
+    }
+    
   }
-  // LEVE TLACITKO
+  // PRAVE TLACITKO
 
   if (temp > 505 && temp < 515)
   {
-    snakeDirection = 1;
+        
+    if (snakeDirection == 3)
+    {
+        snakeDirection = 0;
+    }else{
+        snakeDirection++;
+    }
   }
 
   // STREDNI TLACITKO
   if (temp > 695 && temp < 705)
   {
-    snakeDirection = 2;
+
   }
   delay(500);
-  oled.clear();
+  oled.setFont(FONT8X16);oled.setCursor(40, 1);oled.print(snakeDirection);
+
 }
